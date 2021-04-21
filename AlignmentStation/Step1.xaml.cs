8d9927f923010229ec4e174b3602d5c49f6cdb4f
Al Madireddy <al.madireddy@gmail.com>
2021-04-21 12:19:07 -0500

    connect to thorlabs and measure power

--- AlignmentStation/Step1.xaml.cs
+++ AlignmentStation/Step1.xaml.cs
@@ -14,6 +14,12 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Aerotech.A3200;
 using Aerotech.A3200.Exceptions;
+using System.Diagnostics;
+using System.IO;
+using Thorlabs.TLPM_64.Interop;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Reflection;
 
 namespace AlignmentStation
 {
@@ -22,16 +28,77 @@ namespace AlignmentStation
     /// </summary>
     public partial class Step1 : Page
     {
-        Controller c;
-
         public Step1()
         {
             InitializeComponent();
-            c = Controller.Connect();
         }
 
         private void Do_Task_Click(object sender, RoutedEventArgs e)
         {
+            HandleRef Instrument_Handle = new HandleRef();
+
+            TLPM searchDevice = new TLPM(Instrument_Handle.Handle);
+
+            uint count = 0;
+
+            string firstPowermeterFound = "";
+
+            try
+            {
+                int pInvokeResult = searchDevice.findRsrc(out count);
+
+                if (count > 0)
+                {
+                    StringBuilder descr = new StringBuilder(1024);
+
+                    searchDevice.getRsrcName(0, descr);
+
+                    firstPowermeterFound = descr.ToString();
+                }
+            }
+            catch { }
+
+            if (count == 0)
+            {
+                searchDevice.Dispose();
+                Debug.WriteLine("No power meter could be found.");
+                return;
+            }
+
+            //always use true for ID Query
+            TLPM device = new TLPM(firstPowermeterFound, true, false);  //  For valid Ressource_Name see NI-Visa documentation.
+            Debug.WriteLine("Power meter found: {0}", firstPowermeterFound);
+
+            double measuredPower = 0;
+            short unit = 0;
+            
+            device.getPowerUnit(out unit);
+            device.measPower(out measuredPower);
+
+            Debug.WriteLine("Measured Power: {0}", measuredPower);
+            Debug.WriteLine("Measured Power unit: {0}", unit);
+
+            device.Dispose();
+        }
+
+        private static void autoset(TLPM device)
+        {
+            //Set to PEAK mode for peak search/ autoset
+            device.setFreqMode(1);
+
+            device.startPeakDetector();
+
+            System.Threading.Thread.Sleep(1000);
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                device.isPeakDetectorRunning(out isRunning);
+            }
+
+            //Set to CW mode for normal measurement
+            device.setFreqMode(0);
         }
     }
  }
